fc4a5488b3e17bb8f44a8dada77273dc7ab42d19
lyx0 <stefan.parfuss@protonmail.ch>
2021-02-25 16:07:48 +0100

    smol change

--- hello-world/v2/hello_test.go
+++ hello-world/v2/hello_test.go
@@ -14,18 +14,12 @@ func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Chris")
 		want := "Hello, Chris"
-
-		if got != want {
-			assertCorrectMessage(t, got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
-
-		if got != want {
-			assertCorrectMessage(t, got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 }

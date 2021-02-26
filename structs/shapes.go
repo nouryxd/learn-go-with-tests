9c822d238dd17fefd942b663e802a040a86e2532
lyx0 <stefan.parfuss@protonmail.ch>
2021-02-26 16:49:38 +0100

    add shapes

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,14 +1,26 @@
 package main
 
+import (
+	"math"
+)
+
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
 }
 
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }

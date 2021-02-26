bf4913e0124048ee5191dc99d448b76519628099
lyx0 <stefan.parfuss@protonmail.ch>
2021-02-26 14:08:19 +0100

    add structs

---
+++ structs/shapes.go
@@ -0,0 +1,9 @@
+package main
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
+}

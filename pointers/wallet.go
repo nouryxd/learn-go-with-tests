a7b5378671e97b73d47403eba75df7fa757f0e40
lyx0 <stefan.parfuss@protonmail.ch>
2021-02-26 19:03:27 +0100

    add pointers

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,18 +1,37 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
-type Wallet struct {
-	balance int
+type Bitcoin int
+
+type Stringer interface {
+	String() string
 }
 
-func (w *Wallet) Deposit(amount int) {
-	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return errors.New("oh no")
+	}
+
+	w.balance -= amount
+	return nil
 }

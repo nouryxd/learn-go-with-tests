9c219e4c124426841ee1a92bfbc72486826b1f45
lyx0 <stefan.parfuss@protonmail.ch>
2021-02-28 18:34:28 +0100

    add maps

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -19,6 +19,29 @@ func TestSearch(t *testing.T) {
 	})
 }
 
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	word := "test"
+	definition := "this is just a test"
+
+	dictionary.Add(word, definition)
+
+	assertDefinition(t, dictionary, word, definition)
+}
+
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	if definition != got {
+		t.Errorf("got %q want %q", got, definition)
+	}
+}
+
 func assertError(t testing.TB, got, want error) {
 	t.Helper()
 

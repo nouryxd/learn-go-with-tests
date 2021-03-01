6bfd09925868a98bd9474c863aa038e14a5ce96a
lyx0 <stefan.parfuss@protonmail.ch>
2021-03-01 15:49:09 +0100

    add mocking

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -1,15 +1,21 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
 	"os"
 )
 
-func Countdown(out *bytes.Buffer) {
+const finalWord = "Go!"
+const countdownStart = 3
 
+func Countdown(out io.Writer) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+	}
+	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-	Countdown()
+	Countdown(os.Stdout)
 }

3db13d85bdfbb4e583e64cd12228d9a39b99aeb8
lyx0 <stefan.parfuss@protonmail.ch>
2021-03-02 20:52:23 +0100

    add concurrency

--- concurrency/check_websites_test.go
+++ concurrency/check_websites_test.go
@@ -1,9 +1,33 @@
 package concurrency
 
 import (
+	"reflect"
 	"testing"
 )
 
-func TestCheckWebsites(t *testing.T) {
+func mockWebsiteChecker(url string) bool {
+	if url == "waat://furhurterwe.geds" {
+		return false
+	}
+	return true
+}
 
+func TestCheckWebsite(t *testing.T) {
+	websites := []string{
+		"http://google.com",
+		"http://blog.gypsydave5.com",
+		"waat://furhurterwe.geds",
+	}
+
+	want := map[string]bool{
+		"http://google.com":          true,
+		"http://blog.gypsydave5.com": true,
+		"waat://furhurterwe.geds":    false,
+	}
+
+	got := CheckWebsites(mockWebsiteChecker, websites)
+
+	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("wanted %v, got %v", want, got)
+	}
 }

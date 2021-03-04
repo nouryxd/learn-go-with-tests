d595d711728f7ec5991b6b9795dadf61cca5be78
lyx0 <stefan.parfuss@protonmail.ch>
2021-03-04 23:46:22 +0100

    add context

--- context/context.go
+++ context/context.go
@@ -1,4 +1,4 @@
-package main
+package context
 
 import (
 	"fmt"
@@ -7,6 +7,7 @@ import (
 
 type Store interface {
 	Fetch() string
+	Cancel()
 }
 
 func Server(store Store) http.HandlerFunc {
---
+++ context/testdoubles.go
@@ -0,0 +1,20 @@
+package context
+
+import (
+	"testing"
+	"time"
+)
+
+type SpyStore struct {
+	response  string
+	cancelled bool
+}
+
+func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
+	return s.response
+}
+
+func (s *SpyStore) Cancel() {
+	s.cancelled = true
+}

6ebadf048f02c084e9f5ffab289686ecac30b0fd
lyx0 <stefan.parfuss@protonmail.ch>
2021-03-05 19:39:37 +0100

    refactor numerals to add 4

--- roman-numerals/numeral.go
+++ roman-numerals/numeral.go
@@ -1,8 +1,20 @@
 package numeral
 
+import (
+	"strings"
+)
+
 func ConvertToRoman(arabic int) string {
-	if arabic == 2 {
-		return "II"
+
+	var result strings.Builder
+
+	for i := arabic; i > 0; i-- {
+		if i == 4 {
+			result.WriteString("IV")
+			break
+		}
+		result.WriteString("I")
 	}
-	return "I"
+
+	return result.String()
 }

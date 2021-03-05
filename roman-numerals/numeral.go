591ca88b7fac4e08045f234faf76b6d455eb1bdd
lyx0 <stefan.parfuss@protonmail.ch>
2021-03-05 19:13:31 +0100

    refactor numerals

--- roman-numerals/numeral.go
+++ roman-numerals/numeral.go
@@ -1,5 +1,8 @@
 package numeral
 
 func ConvertToRoman(arabic int) string {
-	return ""
+	if arabic == 2 {
+		return "II"
+	}
+	return "I"
 }
--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -3,10 +3,22 @@ package numeral
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
-	got := ConvertToRoman(1)
-	want := "I"
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
+	cases := []struct {
+		Description string
+		Arabic      int
+		Want        string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to II", 2, "II"},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
+			if got != test.Want {
+				t.Errorf("got %q, want %q", got, test.Want)
+			}
+		})
 	}
 }

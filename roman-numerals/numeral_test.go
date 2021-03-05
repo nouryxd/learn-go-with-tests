330618b017d37f8b712e38eb92f0f3d4d556e608
lyx0 <stefan.parfuss@protonmail.ch>
2021-03-05 21:17:20 +0100

    refactor numerals

--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -1,7 +1,10 @@
 package numeral
 
-import "fmt"
-import "testing"
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
 
 func TestRomanNumerals(t *testing.T) {
 
@@ -48,4 +51,39 @@ func TestRomanNumerals(t *testing.T) {
 			}
 		})
 	}
+}
+
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+var RomanNumerals = []RomanNumeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+func ConvertToRoman(arabic int) string {
+
+	var result strings.Builder
+
+	for _, numeral := range RomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
+		}
+	}
+
+	return result.String()
 }

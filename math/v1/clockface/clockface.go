cc0e118a754ec9f0bba4ad13c042c202d714b4e5
lyx0 <stefan.parfuss@protonmail.ch>
2021-03-13 02:01:46 +0100

    add math

---
+++ math/v1/clockface/clockface.go
@@ -0,0 +1,14 @@
+package clockface
+
+import (
+	"time"
+)
+
+type Point struct {
+	X float64
+	Y float64
+}
+
+func SecondHand(t time.Time) Point {
+	return Point{150, 60}
+}
---
+++ math/v1/clockface/clockface_test.go
@@ -0,0 +1,18 @@
+package clockface_test
+
+import (
+	"github.com/lyx0/learn-go-with-tests/math/v1/clockface"
+	"testing"
+	"time"
+)
+
+func TestSecondHandAtMidnight(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+	want := clockface.Point{X: 150, Y: 150 - 90}
+	got := clockface.SecondHand(tm)
+
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
